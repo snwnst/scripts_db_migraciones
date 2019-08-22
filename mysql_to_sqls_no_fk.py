39e73a348d0ced02141e21c8cb6fd75faaee6b46
Nestor Martinez Hernandez <nestor-mh@outlook.com>
2019-08-22 16:54:27 -0500

    null var

--- mysql_to_sqls_no_fk.py
+++ mysql_to_sqls_no_fk.py
@@ -3,49 +3,21 @@ import mysql.connector
 import pyodbc 
 import datetime
 
-MysqlHost = ''
-MysqlDatabase = ''
-MysqlUser = ''
-MysqlPassword = ''
-
-SqlsHost = ''
-SqlsDatabase = ''
-SqlsUser = ''
-SqlsPassword = ''
-
 scheme = ''
 
-mysqlconnection = mysql.connector.connect(host=MysqlHost,database=MysqlDatabase,user=MysqlUser,password=MysqlPassword)
+mysqlconnection = mysql.connector.connect(host='',database='',user='',password='')
 mysql = mysqlconnection.cursor()
 
-msqlsconnection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER={};DATABASE={};UID={};PWD={}'.format(SqlsHost,SqlsDatabase,SqlsUser,SqlsPassword))
+msqlsconnection = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=;DATABASE=;UID=;PWD=")
 msqls = msqlsconnection.cursor() 
 
 mysql.execute("SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema='{}'".format(scheme))
 tablenames = mysql.fetchall()
 
-def insert_data(table_name):
-    mysql.execute("DESCRIBE {}.{}".format(scheme,table_name))
-    columnames = mysql.fetchall()
-    query = 'INSERT INTO {}.{} ('.format(scheme,table_name)
-    for columname in columnames:
-        query = query+columname[0]+','
-    query = query+')'
-    query = query.replace(",)", ")")
-    mysql.execute("SELECT * FROM {}.{}".format(scheme,table_name))
-    rows = mysql.fetchall()
-    persent_rows_table = tqdm(rows)
-    for row in persent_rows_table:
-        if 'datetime.date' in str(row):
-            for idx, subrow in enumerate(row):
-                if isinstance(subrow, datetime.date) == True:
-                    lst = list(row)
-                    lst[idx] = str(subrow)
-                    row = tuple(lst)
-        msqls.execute(query+" VALUES "+str(row).replace("None", "NULL"))
-        msqlsconnection.commit()
-        persent_rows_table.set_description("MIGRANDO %s" % table_name)
-        
+array_table_names = []
+for tablename in tablenames:
+    create_table(tablename[0])
+
 def create_table(table_name):
     cratetablestr = 'CREATE TABLE {}.{} ('.format(scheme,table_name)
     mysql.execute("DESCRIBE {}.{}".format(scheme,table_name))
@@ -71,11 +43,26 @@ def create_table(table_name):
     msqls.execute(cratetablestr)
     msqlsconnection.commit()
     insert_data(table_name)
-    
-array_table_names = []
-for tablename in tablenames:
-    array_table_names.append(tablename[0])
-    
-persent_create_table = array_table_names
-for table_name in persent_create_table:
-    create_table(table_name)
+
+def insert_data(table_name):
+    mysql.execute("DESCRIBE {}.{}".format(scheme,table_name))
+    columnames = mysql.fetchall()
+    query = 'INSERT INTO {}.{} ('.format(scheme,table_name)
+    for columname in columnames:
+        query = query+columname[0]+','
+    query = query+')'
+    query = query.replace(",)", ")")
+    mysql.execute("SELECT * FROM {}.{}".format(scheme,table_name))
+    rows = mysql.fetchall()
+    persent_rows_table = tqdm(rows)
+    for row in persent_rows_table:
+        if 'datetime.date' in str(row):
+            for idx, subrow in enumerate(row):
+                if isinstance(subrow, datetime.date) == True:
+                    lst = list(row)
+                    lst[idx] = str(subrow)
+                    row = tuple(lst)
+        msqls.execute(query+" VALUES "+str(row).replace("None", "NULL"))
+        msqlsconnection.commit()
+        persent_rows_table.set_description("MIGRANDO %s" % table_name)
+        
